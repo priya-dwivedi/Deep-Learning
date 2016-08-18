e61158d1feb901a55f491a1655dcfb3c70b89216
Priyanka Dwivedi <priyanka.kochhar3@gmail.com>
2016-08-18 16:08:36 -0400

    Add files via upload

---
+++ RNN-Python only-DennyBritz.py
@@ -0,0 +1,337 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 18 10:00:07 2016
+
+@author: s6324900
+"""
+import csv
+import itertools
+import operator
+import numpy as np
+import nltk
+import sys
+from datetime import datetime
+#from utils import *
+
+import matplotlib.pyplot as plt
+
+# Download NLTK model data (you need to do this once)
+nltk.download("book")
+
+# Training data preprocessing.
+# https://github.com/dennybritz/rnn-tutorial-rnnlm/blob/master/RNNLM.ipynb
+vocabulary_size = 8000
+unknown_token = "UNKNOWN_TOKEN"
+sentence_start_token = "SENTENCE_START"
+sentence_end_token = "SENTENCE_END"
+
+# Read the data and append SENTENCE_START and SENTENCE_END tokens
+import os
+os.chdir(r"C:\Users\s6324900\Desktop\Deep learning\RNN")
+print "Reading CSV file..."
+with open('reddit-comments-2015-08.csv', 'rb') as f:
+    reader = csv.reader(f, skipinitialspace=True)
+    reader.next()
+    # Split full comments into sentences
+    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+    # Append SENTENCE_START and SENTENCE_END
+    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+print "Parsed %d sentences." % (len(sentences))
+    
+# Tokenize the sentences into words
+tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+# Count the word frequencies
+word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+print "Found %d unique words tokens." % len(word_freq.items())
+
+# Get the most common words and build index_to_word and word_to_index vectors
+vocab = word_freq.most_common(vocabulary_size-1)
+index_to_word = [x[0] for x in vocab]
+index_to_word.append(unknown_token)
+word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+
+print "Using vocabulary size %d." % vocabulary_size
+print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
+
+# Replace all words not in our vocabulary with the unknown token
+for i, sent in enumerate(tokenized_sentences):
+    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+
+print "\nExample sentence: '%s'" % sentences[1]
+print "\nExample sentence after Pre-processing: '%s'" % tokenized_sentences[1]
+
+# Create the training data
+X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
+y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+
+# Print an training data example
+x_example, y_example = X_train[17], y_train[17]
+print "x:\n%s\n%s" % (" ".join([index_to_word[x] for x in x_example]), x_example)
+print "\ny:\n%s\n%s" % (" ".join([index_to_word[x] for x in y_example]), y_example)
+
+## Initialize the vector
+# word_dim: size of our vocabulary - 8000
+#hidden_dim: no. of hidden layers - 100
+
+class RNNNumpy:
+    
+    def __init__(self, word_dim, hidden_dim=100, bptt_truncate=4):
+        # Assign instance variables
+        self.word_dim = word_dim
+        self.hidden_dim = hidden_dim
+        self.bptt_truncate = bptt_truncate
+        # Randomly initialize the network parameters
+        self.U = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
+        self.V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
+        self.W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+        
+# Define: Forward propogation
+def forward_propagation(self, x):
+    # The total number of time steps
+    T = len(x)
+    # During forward propagation we save all hidden states in s because need them later.
+    # We add one additional element for the initial hidden, which we set to 0
+    s = np.zeros((T + 1, self.hidden_dim))
+    s[-1] = np.zeros(self.hidden_dim)
+    # The outputs at each time step. Again, we save them for later.
+    o = np.zeros((T, self.word_dim))
+    # For each time step...
+    for t in np.arange(T):
+        # Note that we are indxing U by x[t]. This is the same as multiplying U with a one-hot vector.
+        s[t] = np.tanh(self.U[:,x[t]] + self.W.dot(s[t-1]))
+        o[t] = softmax(self.V.dot(s[t]))
+    return [o, s]
+
+RNNNumpy.forward_propagation = forward_propagation
+# Find the most likely next word based on prediction
+
+def predict(self, x):
+    # Perform forward propagation and return index of the highest score
+    o, s = self.forward_propagation(x)
+    return np.argmax(o, axis=1)
+
+RNNNumpy.predict = predict
+
+# See an example output
+
+np.random.seed(10)
+model = RNNNumpy(vocabulary_size)
+o, s = model.forward_propagation(X_train[10])
+print o.shape
+print o
+
+
+predictions = model.predict(X_train[10])
+print predictions.shape
+print predictions
+
+# Calculating the loss
+
+def calculate_total_loss(self, x, y):
+    L = 0
+    # For each sentence...
+    for i in np.arange(len(y)):
+        o, s = self.forward_propagation(x[i])
+        # We only care about our prediction of the "correct" words
+        correct_word_predictions = o[np.arange(len(y[i])), y[i]]
+        # Add to the loss based on how off we were
+        L += -1 * np.sum(np.log(correct_word_predictions))
+    return L
+
+def calculate_loss(self, x, y):
+    # Divide the total loss by the number of training examples
+    N = np.sum((len(y_i) for y_i in y))
+    return self.calculate_total_loss(x,y)/N
+
+RNNNumpy.calculate_total_loss = calculate_total_loss
+RNNNumpy.calculate_loss = calculate_loss
+
+# Calculate baseline loss. Limit to 1000 examples to save time
+print "Expected Loss for random predictions: %f" % np.log(vocabulary_size)
+print "Actual loss: %f" % model.calculate_loss(X_train[:1000], y_train[:1000])
+
+##Training the RNN with SGD and Backpropagation Through Time (BPTT)
+
+def bptt(self, x, y):
+    T = len(y)
+    # Perform forward propagation
+    o, s = self.forward_propagation(x)
+    # We accumulate the gradients in these variables
+    dLdU = np.zeros(self.U.shape)
+    dLdV = np.zeros(self.V.shape)
+    dLdW = np.zeros(self.W.shape)
+    delta_o = o
+    delta_o[np.arange(len(y)), y] -= 1.
+    # For each output backwards...
+    for t in np.arange(T)[::-1]:
+        dLdV += np.outer(delta_o[t], s[t].T)
+        # Initial delta calculation
+        delta_t = self.V.T.dot(delta_o[t]) * (1 - (s[t] ** 2))
+        # Backpropagation through time (for at most self.bptt_truncate steps)
+        for bptt_step in np.arange(max(0, t-self.bptt_truncate), t+1)[::-1]:
+            # print "Backpropagation step t=%d bptt step=%d " % (t, bptt_step)
+            dLdW += np.outer(delta_t, s[bptt_step-1])              
+            dLdU[:,x[bptt_step]] += delta_t
+            # Update delta for next step
+            delta_t = self.W.T.dot(delta_t) * (1 - s[bptt_step-1] ** 2)
+    return [dLdU, dLdV, dLdW]
+
+RNNNumpy.bptt = bptt
+
+## Implement gradient checking to check if bptt is working. Complicated math
+
+
+def gradient_check(self, x, y, h=0.001, error_threshold=0.01):
+    # Calculate the gradients using backpropagation. We want to checker if these are correct.
+    bptt_gradients = model.bptt(x, y)
+    # List of all parameters we want to check.
+    model_parameters = ['U', 'V', 'W']
+    # Gradient check for each parameter
+    for pidx, pname in enumerate(model_parameters):
+        # Get the actual parameter value from the mode, e.g. model.W
+        parameter = operator.attrgetter(pname)(self)
+        print "Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape))
+        # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
+        it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
+        while not it.finished:
+            ix = it.multi_index
+            # Save the original value so we can reset it later
+            original_value = parameter[ix]
+            # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
+            parameter[ix] = original_value + h
+            gradplus = model.calculate_total_loss([x],[y])
+            parameter[ix] = original_value - h
+            gradminus = model.calculate_total_loss([x],[y])
+            estimated_gradient = (gradplus - gradminus)/(2*h)
+            # Reset parameter to original value
+            parameter[ix] = original_value
+            # The gradient for this parameter calculated using backpropagation
+            backprop_gradient = bptt_gradients[pidx][ix]
+            # calculate The relative error: (|x - y|/(|x| + |y|))
+            relative_error = np.abs(backprop_gradient - estimated_gradient)/(np.abs(backprop_gradient) + np.abs(estimated_gradient))
+            # If the error is to large fail the gradient check
+            if relative_error > error_threshold:
+                print "Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix)
+                print "+h Loss: %f" % gradplus
+                print "-h Loss: %f" % gradminus
+                print "Estimated_gradient: %f" % estimated_gradient
+                print "Backpropagation gradient: %f" % backprop_gradient
+                print "Relative Error: %f" % relative_error
+                return 
+            it.iternext()
+        print "Gradient check for parameter %s passed." % (pname)
+
+RNNNumpy.gradient_check = gradient_check
+
+# To avoid performing millions of expensive calculations we use a smaller vocabulary size for checking.
+grad_check_vocab_size = 100
+np.random.seed(10)
+model = RNNNumpy(grad_check_vocab_size, 10, bptt_truncate=1000)
+model.gradient_check([0,1,2,3], [1,2,3,4])
+
+## Since we can perform gradient checking, we can now implement SGD
+
+# Performs one step of SGD.
+def numpy_sdg_step(self, x, y, learning_rate):
+    # Calculate the gradients
+    dLdU, dLdV, dLdW = self.bptt(x, y)
+    # Change parameters according to gradients and learning rate
+    self.U -= learning_rate * dLdU
+    self.V -= learning_rate * dLdV
+    self.W -= learning_rate * dLdW
+
+RNNNumpy.sgd_step = numpy_sdg_step
+
+# Outer SGD Loop
+# - model: The RNN model instance
+# - X_train: The training data set
+# - y_train: The training data labels
+# - learning_rate: Initial learning rate for SGD
+# - nepoch: Number of times to iterate through the complete dataset
+# - evaluate_loss_after: Evaluate the loss after this many epochs
+def train_with_sgd(model, X_train, y_train, learning_rate=0.005, nepoch=100, evaluate_loss_after=5):
+    # We keep track of the losses so we can plot them later
+    losses = []
+    num_examples_seen = 0
+    for epoch in range(nepoch):
+        # Optionally evaluate the loss
+        if (epoch % evaluate_loss_after == 0):
+            loss = model.calculate_loss(X_train, y_train)
+            losses.append((num_examples_seen, loss))
+            time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print "%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, loss)
+            # Adjust the learning rate if loss increases
+            if (len(losses) > 1 and losses[-1][1] > losses[-2][1]):
+                learning_rate = learning_rate * 0.5  
+                print "Setting learning rate to %f" % learning_rate
+            sys.stdout.flush()
+        # For each training example...
+        for i in range(len(y_train)):
+            # One SGD step
+            model.sgd_step(X_train[i], y_train[i], learning_rate)
+            num_examples_seen += 1
+
+## Done! Let's try to get a sense of how long it would take to
+## train our network:
+
+np.random.seed(10)
+model = RNNNumpy(vocabulary_size)
+%timeit model.sgd_step(X_train[10], y_train[10], 0.005)
+
+np.random.seed(10)
+# Train on a small subset of the data to see what happens
+model = RNNNumpy(vocabulary_size)
+losses = train_with_sgd(model, X_train[:100], y_train[:100], nepoch=10, evaluate_loss_after=1)
+
+
+def generate_sentence(model):
+    # We start the sentence with the start token
+    new_sentence = [word_to_index[sentence_start_token]]
+    # Repeat until we get an end token
+    while not new_sentence[-1] == word_to_index[sentence_end_token]:
+        next_word_probs = model.forward_propagation(new_sentence)
+        sampled_word = word_to_index[unknown_token]
+        # We don't want to sample unknown words
+        while sampled_word == word_to_index[unknown_token]:
+            samples = np.random.multinomial(1, next_word_probs[-1])
+            sampled_word = np.argmax(samples)
+        new_sentence.append(sampled_word)
+    sentence_str = [index_to_word[x] for x in new_sentence[1:-1]]
+    return sentence_str
+
+num_sentences = 10
+senten_min_length = 7
+
+for i in range(num_sentences):
+    sent = []
+    # We want long sentences, not sentences with one or two words
+    while len(sent) < senten_min_length:
+        sent = generate_sentence(model)
+    print " ".join(sent)
+
+## Evaluting on Theano with CPU
+
+import os
+os.environ["THEANO_FLAGS"] = "mode=FAST_RUN,device=cpu,floatX=float32"
+
+import theano
+import theano.tensor as T
+from utils import *
+
+from rnn_theano import RNNTheano, gradient_check_theano
+
+np.random.seed(10)
+# To avoid performing millions of expensive calculations we use a smaller vocabulary size for checking.
+grad_check_vocab_size = 5
+model = RNNTheano(grad_check_vocab_size, 10)
+gradient_check_theano(model, [0,1,2,3], [1,2,3,4])
+
+## Without Theano in fast mode it took 1m 12s. Python alone took 750 ms
+np.random.seed(20)
+model = RNNTheano(vocabulary_size)
+%timeit model.sgd_step(X_train[10], y_train[10], 0.005)

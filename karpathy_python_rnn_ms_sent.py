669dce4771197c7b4d64945f156e869b920dff06
Priyanka Dwivedi <priyanka.kochhar3@gmail.com>
2016-08-18 09:37:33 -0400

    Add files via upload

---
+++ karpathy_python_rnn_ms_sent.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 15:38:16 2016
+
+@author: s6324900
+"""
+
+"""
+Minimal character-level Vanilla RNN model. Written by Andrej Karpathy (@karpathy)
+BSD License
+"""
+
+### Oh wow! I made this work. Successfuly imported MS Sent to Excel.
+## Deleted unwanted columns in excel and converted back to text
+
+import os
+os.chdir(r"C:\Users\s6324900\Desktop\Deep learning\RNN")
+
+import xlrd
+wb = xlrd.open_workbook('ms_sent_mail.xls')
+sh= wb.sheet_by_name("Email")
+print(sh.nrows)
+print(sh.ncols)
+
+n=0
+i=0
+unicodeToAsciiMap = {u'\u2019':"'", u'\u2018':"`", }
+with open("ms_sent_mail.txt","a") as my_file:
+    for n in range(sh.nrows):
+        for i in range(sh.ncols):
+            Load =sh.cell_value(n,i)
+            DB1 = Load + ""
+            try:
+                my_file.write(DB1 + '/n')
+            except:
+                if unicodeToAsciiMap.has_key(Load):
+                    Load = unicodeToAsciiMap[i]
+                    my_file.write(Load + '/n')
+
+
+import numpy as np
+
+# data I/O
+data = open('ms_sent_mail.txt', 'r').read() # should be simple plain text file
+chars = list(set(data))
+data_size, vocab_size = len(data), len(chars)
+print 'data has %d characters, %d unique.' % (data_size, vocab_size)
+char_to_ix = { ch:i for i,ch in enumerate(chars) }
+ix_to_char = { i:ch for i,ch in enumerate(chars) }
+
+# hyperparameters
+hidden_size = 100 # size of hidden layer of neurons
+seq_length = 25 # number of steps to unroll the RNN for
+learning_rate = 1e-1
+
+# model parameters
+Wxh = np.random.randn(hidden_size, vocab_size)*0.01 # input to hidden
+Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
+Why = np.random.randn(vocab_size, hidden_size)*0.01 # hidden to output
+bh = np.zeros((hidden_size, 1)) # hidden bias
+by = np.zeros((vocab_size, 1)) # output bias
+
+def lossFun(inputs, targets, hprev):
+  """
+  inputs,targets are both list of integers.
+  hprev is Hx1 array of initial hidden state
+  returns the loss, gradients on model parameters, and last hidden state
+  """
+  xs, hs, ys, ps = {}, {}, {}, {}
+  hs[-1] = np.copy(hprev)
+  loss = 0
+  # forward pass
+  for t in xrange(len(inputs)):
+    xs[t] = np.zeros((vocab_size,1)) # encode in 1-of-k representation
+    xs[t][inputs[t]] = 1
+    hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t-1]) + bh) # hidden state
+    ys[t] = np.dot(Why, hs[t]) + by # unnormalized log probabilities for next chars
+    ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t])) # probabilities for next chars
+    loss += -np.log(ps[t][targets[t],0]) # softmax (cross-entropy loss)
+  # backward pass: compute gradients going backwards
+  dWxh, dWhh, dWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
+  dbh, dby = np.zeros_like(bh), np.zeros_like(by)
+  dhnext = np.zeros_like(hs[0])
+  for t in reversed(xrange(len(inputs))):
+    dy = np.copy(ps[t])
+    dy[targets[t]] -= 1 # backprop into y. see http://cs231n.github.io/neural-networks-case-study/#grad if confused here
+    dWhy += np.dot(dy, hs[t].T)
+    dby += dy
+    dh = np.dot(Why.T, dy) + dhnext # backprop into h
+    dhraw = (1 - hs[t] * hs[t]) * dh # backprop through tanh nonlinearity
+    dbh += dhraw
+    dWxh += np.dot(dhraw, xs[t].T)
+    dWhh += np.dot(dhraw, hs[t-1].T)
+    dhnext = np.dot(Whh.T, dhraw)
+  for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
+    np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients
+  return loss, dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs)-1]
+
+def sample(h, seed_ix, n):
+  """ 
+  sample a sequence of integers from the model 
+  h is memory state, seed_ix is seed letter for first time step
+  """
+  x = np.zeros((vocab_size, 1))
+  x[seed_ix] = 1
+  ixes = []
+  for t in xrange(n):
+    h = np.tanh(np.dot(Wxh, x) + np.dot(Whh, h) + bh)
+    y = np.dot(Why, h) + by
+    p = np.exp(y) / np.sum(np.exp(y))
+    ix = np.random.choice(range(vocab_size), p=p.ravel())
+    x = np.zeros((vocab_size, 1))
+    x[ix] = 1
+    ixes.append(ix)
+  return ixes
+
+n, p = 0, 0
+mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
+mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
+smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
+while True:
+  # prepare inputs (we're sweeping from left to right in steps seq_length long)
+  if p+seq_length+1 >= len(data) or n == 0: 
+    hprev = np.zeros((hidden_size,1)) # reset RNN memory
+    p = 0 # go from start of data
+  inputs = [char_to_ix[ch] for ch in data[p:p+seq_length]]
+  targets = [char_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
+
+  # sample from the model now and then
+  if n % 100 == 0:
+    sample_ix = sample(hprev, inputs[0], 200)
+    txt = ''.join(ix_to_char[ix] for ix in sample_ix)
+    print '----\n %s \n----' % (txt, )
+
+  # forward seq_length characters through the net and fetch gradient
+  loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
+  smooth_loss = smooth_loss * 0.999 + loss * 0.001
+  if n % 100 == 0: print 'iter %d, loss: %f' % (n, smooth_loss) # print progress
+  
+  # perform parameter update with Adagrad
+  for param, dparam, mem in zip([Wxh, Whh, Why, bh, by], 
+                                [dWxh, dWhh, dWhy, dbh, dby], 
+                                [mWxh, mWhh, mWhy, mbh, mby]):
+    mem += dparam * dparam
+    param += -learning_rate * dparam / np.sqrt(mem + 1e-8) # adagrad update
+
+  p += seq_length # move data pointer
+  n += 1 # iteration counter 
+  
+# gradient checking
+from random import uniform
+def gradCheck(inputs, target, hprev):
+  global Wxh, Whh, Why, bh, by
+  num_checks, delta = 10, 1e-5
+  _, dWxh, dWhh, dWhy, dbh, dby, _ = lossFun(inputs, targets, hprev)
+  for param,dparam,name in zip([Wxh, Whh, Why, bh, by], [dWxh, dWhh, dWhy, dbh, dby], ['Wxh', 'Whh', 'Why', 'bh', 'by']):
+    s0 = dparam.shape
+    s1 = param.shape
+    assert s0 == s1, 'Error dims dont match: %s and %s.' % (`s0`, `s1`)
+    print name
+    for i in xrange(num_checks):
+      ri = int(uniform(0,param.size))
+      # evaluate cost at [x + delta] and [x - delta]
+      old_val = param.flat[ri]
+      param.flat[ri] = old_val + delta
+      cg0, _, _, _, _, _, _ = lossFun(inputs, targets, hprev)
+      param.flat[ri] = old_val - delta
+      cg1, _, _, _, _, _, _ = lossFun(inputs, targets, hprev)
+      param.flat[ri] = old_val # reset old value for this parameter
+      # fetch both numerical and analytic gradient
+      grad_analytic = dparam.flat[ri]
+      grad_numerical = (cg0 - cg1) / ( 2 * delta )
+      rel_error = abs(grad_analytic - grad_numerical) / abs(grad_numerical + grad_analytic)
+      print '%f, %f => %e ' % (grad_numerical, grad_analytic, rel_error)
+      # rel_error should be on order of 1e-7 or less
+## After 70K iterations gets to a loss of 30-32. However output is still reasonably basic
+## Computer has learned about the signature and my name. But can't write good emails yet
